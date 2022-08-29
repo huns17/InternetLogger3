91a0944ebc63213d2320446fffbcfef4a83a0ec7
JOO SUNG JAE HANS <sungjaejoo@JOOs-MacBook-Air.local>
2022-08-29 18:14:24 +0800

    Updates

---
+++ precache-manifest.8a9333f91c753018dd5f8e7a0c367405.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443684545c6844147313849fd938a522",
+    "url": "/InternetLogger3/index.html"
+  },
+  {
+    "revision": "6c165c8e0183593b60b9",
+    "url": "/InternetLogger3/static/css/main.c633f605.chunk.css"
+  },
+  {
+    "revision": "f3c7f72d8ef34a356f77",
+    "url": "/InternetLogger3/static/js/2.5a72f3b8.chunk.js"
+  },
+  {
+    "revision": "8d4d6b8ee3a7171aa4848b329f2ee050",
+    "url": "/InternetLogger3/static/js/2.5a72f3b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c165c8e0183593b60b9",
+    "url": "/InternetLogger3/static/js/main.0bba85f7.chunk.js"
+  },
+  {
+    "revision": "f9e1c70553e53e7dfaed",
+    "url": "/InternetLogger3/static/js/runtime-main.0f8119c5.js"
+  }
+]);
