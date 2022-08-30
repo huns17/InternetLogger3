9697ca233480eb8c029f4e084428c3ad6209d027
JOO SUNG JAE HANS <sungjaejoo@JOOs-MacBook-Air.local>
2022-08-31 02:15:50 +0800

    first commit

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SignInform from "../components/SingInForm/SignInForm";
+import SignInform from "../components/SignInForm/SignInForm";
 function SignIn() {
   return <SignInform />;
 }
